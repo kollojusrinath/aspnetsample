048a0a81e3fe89ac47953b1e4ec07f7c68a85726
Kolloju Srinath <kollojusrinth004@gmail.com>
2022-04-29 17:56:48 +0530

    modified files

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
     backend "azurerm" {
         resource_group_name  = "App-rg"
         storage_account_name = "tfstatekolloju"
-        container_name       = "tfstate"
+        container_name       = "tfstate2"
         key                  = "terraform.tfstate"
     }
 }
@@ -19,23 +19,23 @@ variable "imagebuild" {
 
 
 
-resource "azurerm_resource_group" "tf_test" {
+resource "azurerm_resource_group" "tf_test2" {
   name = "tfmainrg"
   location = "Australia East"
 }
 
-resource "azurerm_container_group" "tfcg_test" {
-  name                      = "sampleapp"
+resource "azurerm_container_group" "tfcg_test2" {
+  name                      = "sampleapp2"
   location                  = azurerm_resource_group.tf_test.location
   resource_group_name       = azurerm_resource_group.tf_test.name
 
   ip_address_type     = "public"
-  dns_name_label      = "sampleapp"
+  dns_name_label      = "sampleapp2"
   os_type             = "Linux"
 
   container {
       name            = "sampleapp2"
-      image           = "kubesrinath/sampleapp:${var.imagebuild}"
+      image           = "kubesrinath/sampleapp2:${var.imagebuild}"
         cpu             = "1"
         memory          = "1"
 
